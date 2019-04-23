dfe71860282eb0c1c03213ce3e09c75a6daa9aac
Pavel <pav007prikhod@mail.ru>
2019-04-24 00:21:46 +0700

    Sender and listener v. alpha

--- server.py
+++ server.py
@@ -4,9 +4,9 @@ s = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
 host = "25.34.220.231"
 port = 59980
 s.bind((host, port))
-s.listen(2)
+s.listen(1)
 cs = []
-for i in range(2):
+for i in range(1):О
     clnt, addr = s.accept()
     clnt.setblocking(0)
     cs.append(clnt)
@@ -15,4 +15,4 @@ while True:
     if len(cs) != 0:
         for c in cs:
             c.send(b"MESSAGE!")
-            print(b"Sended")
+            print(b"Sended!")
