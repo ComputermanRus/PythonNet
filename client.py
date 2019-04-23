f09087d57db3eb9ccab61f29fa6f2f0f24ed6396
Pavel <pav007prikhod@mail.ru>
2019-04-24 00:10:36 +0700

    Infinity listener

--- client.py
+++ client.py
@@ -4,9 +4,8 @@ s = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
 host = "25.34.220.231"
 port = 59980
 s.connect((host, port))
-s.send(b'Are you achuel?')
-data = s.recv(1024)
-if data:
-    print("Recieved: ", data)
-z = input()
+while True:
+    data = s.recv(1024)
+    if data:
+        print("Recieved: ", data)
 s.close()
